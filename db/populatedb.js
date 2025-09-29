9babcaaa574bcf364f04b2121e62c1fb6561d3b4
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2025-09-29 17:07:54 -0600

    Edited populatedb script to:     DROPS any tables to reset and repopulate the db     Deleting trainer means pokemon remains, but with trainer_id = NULL     Deleting pokemon removes type mappings with ON DELETE CASCADE     Added more tables (trainers, types, pokemon_type)

--- db/populatedb.js
+++ db/populatedb.js
@@ -4,15 +4,54 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS pokemons (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+DROP TABLE IF EXISTS trainers;
+DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS pokemon_type;
+
+CREATE TABLE IF NOT EXISTS trainers (
+    trainer_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR ( 255 )
+);
+
+INSERT INTO trainers (name)
+VALUES
+    ('Red'),
+    ('Sapphire');
+
+
+
+CREATE TABLE IF NOT EXISTS pokemons (
+    pokemon_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 ),
+    trainer_id INTEGER REFERENCES trainers(trainer_id) ON DELETE SET NULL
 );
 
 INSERT INTO pokemons (name)
 VALUES
     ('Pikachu'),
     ('Charmander');
+
+
+
+
+CREATE TABLE IF NOT EXISTS types (
+    type_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 )
+);
+
+INSERT INTO types (name)
+VALUES
+    ('Fire'),
+    ('Water');
+
+
+
+CREATE TABLE IF NOT EXISTS pokemon_type (
+    pokemon_id INTEGER REFERENCES pokemons(pokemon_id) ON DELETE CASCADE,
+    type_id INTEGER REFERENCES types(type_id) ON DELETE CASCADE,
+    PRIMARY KEY (pokemon_id, type_id)
+);
 `;
 
 async function main() {
